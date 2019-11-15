42cbe6593002b012a67309ab1aa24a719b11dfe8
StumboEugen <marineworld@foxmail.com>
2019-11-15 15:25:25 +0800

    first init of CMake project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by stumbo on 18-5-10.
+//
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
