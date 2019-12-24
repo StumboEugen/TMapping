7b7b6d5d16e573fac43914f51babf942ba2a678f
StumboEugen <marineworld@foxmail.com>
2019-12-24 16:28:36 +0800

    补上一次commit缺失文件

---
+++ include/tmapping/UI/UITools.h
@@ -0,0 +1,36 @@
+//
+// Created by stumbo on 2019/12/23.
+//
+
+#ifndef TMAPPING_INCLUDE_TMAPPING_UI_UITOOLS_H
+#define TMAPPING_INCLUDE_TMAPPING_UI_UITOOLS_H
+
+#include "tmapping/tools/TopoVec2.h"
+
+#include <QPointF>
+
+namespace tmap {
+
+static constexpr double METER2PIX = 60.0;
+
+/// UI TOOLS
+namespace UIT {
+inline TopoVec2 QPt2TopoVec(const QPointF& qPoint)
+{
+    return {qPoint.x() / METER2PIX, qPoint.y() / -METER2PIX};
+}
+
+inline QPointF TopoVec2QPt(const TopoVec2& topoVec)
+{
+    return {topoVec.px * METER2PIX, topoVec.py * -METER2PIX};
+}
+
+inline double QMeter(double meter)
+{
+    return METER2PIX * meter;
+}
+} /// namespace UIT
+
+}
+
+#endif //TMAPPING_INCLUDE_TMAPPING_UI_UITOOLS_H
